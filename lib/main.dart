8ae4a8917df548d1f1bcabd386478deefc2e036c
HP <rajkhatri23066@gmail.com>
2023-01-18 17:05:49 +0530

    smile

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Align(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 150,
+                ),
+                Container(
+                  height: 350,
+                  decoration: BoxDecoration(
+                    // color: Colors.white,
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: Colors.orange,
+                      width: 40,
+                    ),
+                  ),
+                  alignment: const Alignment(0, -5),
+                  child: Container(
+                    height: 262,
+                    decoration: const BoxDecoration(
+                      color: Colors.orange,
+                      shape: BoxShape.circle,
+                    ),
+                    alignment: const Alignment(-0.50, -0.50),
+                    child: Container(
+                      height: 80,
+                      width: 80,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                      ),
+                      alignment: const Alignment(70, 0),
+                      child: Container(
+                        height: 77,
+                        width: 77,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
